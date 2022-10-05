e540447afdda4dac532f1b441a32c912c3480a0a
wil <101148394+wil789@users.noreply.github.com>
2022-10-04 21:15:27 -0400

    v3
    updated to # notes around the code

--- walletminer v3.py
+++ walletminer v3.py
@@ -1,3 +1,6 @@
+# everything here is fake and doesnt do anything. uses 2-3% of gpu and no cpu usage.
+# dont try to sell this, because i will find you.
+
 #setup
 import time
 import random
@@ -21,11 +24,11 @@ def g(rolls):
 
 def print_slow(str):
     for letter in str:
-        sys.stdout.write(letter)
+        sys.stdout.write(letter)         #not used
         sys.stdout.flush()
         time.sleep(0.00001)
-
-kk = input("What is your product key?\n>")
+#-----------------------------------------
+kk = input("What is your product key?\n>")     #product key check
 k = kk.lower()
 
 if k == "women":
@@ -39,12 +42,12 @@ else:
 	print(colorama.Fore.RED + "Product key not valid, console now closing.")
 	time.sleep(5)
 	exit()
-
+#-----------------------------------------
 #interface
 ban = """██╗    ██╗ █████╗ ██╗     ██╗     ███████╗████████╗    ███╗   ███╗██╗███╗   ██╗███████╗██████╗     ██╗   ██╗██████╗ 
 ██║    ██║██╔══██╗██║     ██║     ██╔════╝╚══██╔══╝    ████╗ ████║██║████╗  ██║██╔════╝██╔══██╗    ██║   ██║╚════██╗
 ██║ █╗ ██║███████║██║     ██║     █████╗     ██║       ██╔████╔██║██║██╔██╗ ██║█████╗  ██████╔╝    ██║   ██║ █████╔╝
-██║███╗██║██╔══██║██║     ██║     ██╔══╝     ██║       ██║╚██╔╝██║██║██║╚██╗██║██╔══╝  ██╔══██╗    ╚██╗ ██╔╝██╔═══╝ 
+██║███╗██║██╔══██║██║     ██║     ██╔══╝     ██║       ██║╚██╔╝██║██║██║╚██╗██║██╔══╝  ██╔══██╗    ╚██╗ ██╔╝██╔═══╝  
 ╚███╔███╔╝██║  ██║███████╗███████╗███████╗   ██║       ██║ ╚═╝ ██║██║██║ ╚████║███████╗██║  ██║     ╚████╔╝ ███████╗
  ╚══╝╚══╝ ╚═╝  ╚═╝╚══════╝╚══════╝╚══════╝   ╚═╝       ╚═╝     ╚═╝╚═╝╚═╝  ╚═══╝╚══════╝╚═╝  ╚═╝      ╚═══╝  ╚══════╝
 color list: 'white', 'blue', 'red', 'green', 'cyan', 'purple', 'yellow'
@@ -54,7 +57,7 @@ format: 'type' |0x64| 'balance'
 """
 print(ban)
 #-----------------------------------------
-nn = input("enter the 1st color you want to use here:")  #asking for first color
+nn = input("enter the 1st color you want to use here:")  # asking for first color
 n = nn.lower()
 if n == "blue":
 	col_ur = colorama.Fore.BLUE
@@ -63,7 +66,7 @@ elif n == "yellow":
 elif n == "white":
 	col_ur = colorama.Fore.WHITE
 elif n == "red":
-	col_ur = colorama.Fore.RED       #seclecting first color
+	col_ur = colorama.Fore.RED       # seclecting first color
 elif n == "purple":
 	col_ur = colorama.Fore.MAGENTA
 elif n == "cyan":
@@ -74,7 +77,7 @@ else:
 	print("Input not valid, defaulting to white.")
 	col_ur = colorama.Fore.WHITE
 
-bb = input("enter the 2nd color you want to use here:")   #asking for second color
+bb = input("enter the 2nd color you want to use here:")   # asking for second color
 b = bb.lower()
 #-----------------------------------------
 if b == "blue":
@@ -84,7 +87,7 @@ elif b == "yellow":
 elif b == "white":
 	col_ur2 = colorama.Fore.WHITE
 elif b == "red":
-	col_ur2 = colorama.Fore.RED        #selecting second color
+	col_ur2 = colorama.Fore.RED        # selecting second color
 elif b == "purple":
 	col_ur2 = colorama.Fore.MAGENTA
 elif b == "cyan":
@@ -109,7 +112,7 @@ Bitcoin/Ethereum wallet miner
 What wallet type you want to check?"""
 print(ban1)
 
-tt = input("'btc'/'eth'/'both'\n>")
+tt = input("'btc'/'eth'/'both'\n>")       # asking for the crypto wallet type to mine
 
 t = tt.lower()
 if t == "btc":
@@ -118,7 +121,7 @@ if t == "btc":
 	false_co = "BTC"
 elif t == "eth":
 	coin_co =  "Ethereum"
-	short_co = "0.0000 ETH"
+	short_co = "0.0000 ETH"          # differnt cypto to select from
 	false_co = "ETH"
 elif t == "both":
 	coin_co = "Both"
@@ -129,34 +132,37 @@ else:
 	coin_co = "Bitcoin"
 	short_co = "0.0000 BTC"	
 	false_co = "BTC"
-
+#---------------------------------------------------------------------------------------------------------------------------
 ban2 = """
 ========================================================
 
 What is your wallet pubic key? (to send funds to)
 If you only want the wallet key, leave this feild empty."""
 print(ban2)
-key = input(">")
-print("\nWallet not valid, proceeding in key only mode.")
+key = input(">")       # 'key' var is not used.                                              
+print("\nWallet not valid, proceeding in key only mode.")          # it doesnt actually check if the wallet is real (suprise)
 time.sleep(3)
 os.system('cls')
 print(col_ur  + " █     █░ ▄▄▄       ██▓     ██▓    ▓█████▄▄▄█████▓" + col_ur2 + "  ███▄ ▄███▓ ██▓ ███▄    █ ▓█████  ██▀███  ")
 print(col_ur  + "▓█░ █ ░█░▒████▄    ▓██▒    ▓██▒    ▓█   ▀▓  ██▒ ▓▒" + col_ur2 + " ▓██▒▀█▀ ██▒▓██▒ ██ ▀█   █ ▓█   ▀ ▓██ ▒ ██▒")
 print(col_ur  + "▒█░ █ ░█ ▒██  ▀█▄  ▒██░    ▒██░    ▒███  ▒ ▓██░ ▒░" + col_ur2 + " ▓██    ▓██░▒██▒▓██  ▀█ ██▒▒███   ▓██ ░▄█ ▒")
 print(col_ur  + "░█░ █ ░█ ░██▄▄▄▄██ ▒██░    ▒██░    ▒▓█  ▄░ ▓██▓ ░ " + col_ur2 + " ▒██    ▒██ ░██░▓██▒  ▐▌██▒▒▓█  ▄ ▒██▀▀█▄  ")
-print(col_ur  + "░░██▒██▓  ▓█   ▓██▒░██████▒░██████▒░▒████▒ ▒██▒ ░ " + col_ur2 + " ▒██▒   ░██▒░██░▒██░   ▓██░░▒████▒░██▓ ▒██▒")
+print(col_ur  + "░░██▒██▓  ▓█   ▓██▒░██████▒░██████▒░▒████▒ ▒██▒ ░ " + col_ur2 + " ▒██▒   ░██▒░██░▒██░   ▓██░░▒████▒░██▓ ▒██▒")         # another banner
 print(col_ur  + "░ ▓░▒ ▒   ▒▒   ▓▒█░░ ▒░▓  ░░ ▒░▓  ░░░ ▒░ ░ ▒ ░░   " + col_ur2 + " ░ ▒░   ░  ░░▓  ░ ▒░   ▒ ▒ ░░ ▒░ ░░ ▒▓ ░▒▓░")
 print(col_ur  + "  ▒ ░ ░    ▒   ▒▒ ░░ ░ ▒  ░░ ░ ▒  ░ ░ ░  ░   ░    " + col_ur2 + " ░  ░      ░ ▒ ░░ ░░   ░ ▒░ ░ ░  ░  ░▒ ░ ▒░")
 print(col_ur  + "  ░   ░    ░   ▒     ░ ░     ░ ░      ░    ░      " + col_ur2 + " ░      ░    ▒ ░   ░   ░ ░    ░     ░░   ░ ")
 print(col_ur  + "    ░          ░  ░    ░  ░    ░  ░   ░  ░        " + col_ur2 + "         ░    ░           ░    ░  ░   ░    ")
+#--------------------------------------------------------------------------------------------------------------------------------------------------------------------
 time.sleep(1.25)
-for en in range(1, (random.randint(2000000, 20000000))):
+for en in range(1, (random.randint(2000000, 20000000))):      # randomness of finding a wallet, its 2 million and 20 million.
 	print(col_ur  + coin_co + col_ur2 +" |0x"+g(52) + "| " + col_ur + short_co + "  (INVALID)")
 	time.sleep(0.01)
-
-dawg = ['13vha36jMwcxzLvogef1QKdyHUsnwSrSyi', '1KYGfzr2RrvbT3wjtnWhksUcrPtatZgki6', '16r1hJDrXgHcZWymwFpaLrriqRzxqUA8Vm', '1MFxWN1fvMicg8XFZaEr5EkEs4qWZb59bp', '1BRf6HNxL5riFCuCeYJ3wuSyjjWeLynHQQ']
-derg = ['L3TFkGWsVnherhkq6Qy9j3kr1TjCZzbn1u4sWa3Jag9dBDTsCnCh', 'KxnYWCXyqaArve6hiQ4e3igN6RUbWsSEA1MmWMXip21JCVYoCjt4', 'L5GTYtR2zWvtA6T8jNe9XtnXNHcYMDBGeXMAaPUQ84dnNYRAxLFW']
+#-------------------------------------------------------------------------------------------------------------------------------
+dawg = ['13vha36jMwcxzLvogef1QKdyHUsnwSrSyi', '1KYGfzr2RrvbT3wjtnWhksUcrPtatZgki6', '16r1hJDrXgHcZWymwFpaLrriqRzxqUA8Vm',
+	'1MFxWN1fvMicg8XFZaEr5EkEs4qWZb59bp', '1BRf6HNxL5riFCuCeYJ3wuSyjjWeLynHQQ']                                    #'dawg' is random public keys
+derg = ['L3TFkGWsVnherhkq6Qy9j3kr1TjCZzbn1u4sWa3Jag9dBDTsCnCh', 'KxnYWCXyqaArve6hiQ4e3igN6RUbWsSEA1MmWMXip21JCVYoCjt4',
+	'L5GTYtR2zWvtA6T8jNe9XtnXNHcYMDBGeXMAaPUQ84dnNYRAxLFW']                                                        #'derg' is random private keys (not used)
 print("\nValid adress found, privite key is below.")
 print(col_ur  + coin_co + col_ur2 +" |0xKyx4csZSgXAEssUCtvS8fx1i7jSKD7Aw41cuVhXyTF89r8Ff3K5u| " + col_ur + "0.0000 " + false_co  + "  (VALID)")
-print("The public address is;" + col_ur + " " + (random.choice(dawg)))
+print("The public address is;" + col_ur + " " + (random.choice(dawg)))              # saying that the program has found a wallet, containing nothing
 while True: pass
